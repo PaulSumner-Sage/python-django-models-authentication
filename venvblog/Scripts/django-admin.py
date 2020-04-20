55aab8ca9df45142a7e4be39bff2553686840ec5
Arkadao <arkadczio@protonmail.com>
2020-04-20 10:03:41 +0200

    user authentication

---
+++ venvblog/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\arkadiusz.trzcinski\documents\projects\django\python-django-models-authentication\venvblog\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
