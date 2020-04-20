33371e0acc8c0e27a56f574a1502b3548e87c8ee
Arkadao <arkadczio@protonmail.com>
2020-04-20 10:48:03 +0200

    Add Blog Post Model v1

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,2 +1,12 @@
+from django.conf import settings
 from django.db import models
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE)
+    body = models.TextField()
+    postdate = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__():
+        return self.title
 
